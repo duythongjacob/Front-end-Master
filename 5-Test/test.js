ce3925da142b84cc49163673705f121eb5f7ea6e
duythongjacob <phamduythong@hiast.edu.vn>
2022-03-05 14:28:27 +0700

    1-5
    my new worked

---
+++ 5-Test/test.js
@@ -0,0 +1,37 @@
+var array = [];
+function Items(...items) {
+  array.push(...items);
+}
+function evenAndOdd() {
+
+  var newArray= [] 
+ 
+  for (let i = 0; i < array.length; i++) {
+    
+  
+    if (array.indexOf(array[i]) === i) {
+      
+   
+      newArray.push(array[i])
+      newArray.sort();
+     
+    }
+   
+  }
+  for (let i = 0; i < newArray.length; i++) {
+    if(newArray[i]%2==0){
+      console.log(`${newArray[i]} la so chan`);
+    }
+    else{
+      console.log(`${newArray[i]} la so le`);
+    }
+    if( newArray[i]>=5){
+      console.log(`${newArray[i]} la so lon hon hoac bang  5`);
+    }
+   
+    }
+  }
+ 
+
+Items(5,7,8,9,4,6,7,7,7);
+evenAndOdd();
